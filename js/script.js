892ef56f2aa31246dfbdc22100d0e410262ab503
Nidnorg480 <grondin.stephane@gmail.com>
2018-10-15 17:12:46 +0200

    bla

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
 
-$(document).ready(function () {
+//On ecoute le click 
 
-$('.nav.item').click(function ()  {
+$('.nav.item').click(function (event)  {
     $('.nav.item').removeClass('active');
-    $(this).adClass ("active");
+    $(this).addClass ("active");
     });
 
-});
+//On vide la div qui contient le mot
+var word = $('#tap-text').text().trim();
+
+$('#tap-text').text('');
+
+//Parcourir les lettres du mot 
+for (let i = 0; i < word.length; i++) {
+    console.log(word[i]);
+
+
+setTimeout(function(){
+    console.log(word[i]);
+
+    $("#tap-text").append(word[i]);
+    
+}, 300 * i);
+}
